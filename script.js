c2cf84469b0c4e9a52009da38f72cf6e313fb4d7
praveen1534 <pk8378262@gmail.com>
2024-03-20 08:32:45 +0530

    change

---
+++ script.js
@@ -0,0 +1,32 @@
+// let typed = new Typed('.speedtyping', {
+//     strings: ["First sentence.", "Second sentence."],
+//     typeSpeed: 100,
+//     backSpeed:100,
+//     looped:true
+//   });
+
+let typed=new Typed('.speedtyping',{
+    strings:["FRONT DEVELOPER","DESIGNER"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    looped:true
+}) ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var typed = new Typed('.speedtyping', {
+//     Strings: ["First sentence.", "Second sentence."],
+//     typeSpeed:150,
+//     backSpeed:150,
+//     //  backDelay:1000,
