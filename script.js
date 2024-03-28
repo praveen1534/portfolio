851b3d87a767d51bca8a1a52cab383fc77c128f9
praveen1534 <pk8378262@gmail.com>
2024-03-28 08:52:50 +0530

    change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 let typed=new Typed('.speedtyping',{
-    strings:["FRONT END DEVELOPER","DESIGNER"],
+    strings:["FRONT END DEVELOPER"],
     typeSpeed:150,
     backSpeed:150,
     backDelay:1000,
